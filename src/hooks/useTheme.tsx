2af446bfaf86ea3e51cdbb62ad50910ef4f52b4c
FourteenBrush <74827262+FourteenBrush@users.noreply.github.com>
2025-08-02 20:03:12 +0200

    Abstract away theming hooks with ThemeProvider

---
+++ src/hooks/useTheme.tsx
@@ -0,0 +1,27 @@
+import { darkTheme, lightTheme, ThemeData } from "@/theme"
+import { createContext, PropsWithChildren, useContext } from "react"
+import { useColorScheme } from "react-native"
+
+export const ThemeContext = createContext<ThemeData>(lightTheme)
+
+/** Provides a theme based on the os settings */
+export const ThemeProvider = ({ children }: PropsWithChildren) => {
+  // FIXME: settings override
+  const preferredColorScheme = useColorScheme()
+  const scheme = preferredColorScheme == "dark" ? darkTheme : lightTheme
+  
+  return (
+    <ThemeContext.Provider value={scheme}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export const useTheme = () => {
+  const theme = useContext(ThemeContext)
+  
+  if (theme == null) {
+    throw new Error("ThemeContext provider not found in the widget tree")
+  }
+  return theme
+}
