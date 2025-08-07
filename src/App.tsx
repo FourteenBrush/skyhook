177f1b4137f7ae1ef57dd07c7843faec3d159cf1
FourteenBrush <74827262+FourteenBrush@users.noreply.github.com>
2025-08-08 00:52:53 +0200

    Add SafeAreaView around app wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import LandingPageScreen from "@/screens/LandingPageScreen"
 import MaterialDesignIcons from "@react-native-vector-icons/material-design-icons"
 import { ThemeProvider } from "@/hooks/useTheme"
 import SearchFlightPage from "./screens/SearchFlightPage"
+import { SafeAreaView } from "react-native"
 
 /** Mapping of params needed by route names */
 export type NavParams = {
@@ -15,17 +16,19 @@ const Stack = createNativeStackNavigator<NavParams>()
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <ThemeProvider>
-        <Stack.Navigator initialRouteName="home" screenOptions={{
-          title: "Skyhook",
-          headerBackVisible: true,
-          headerLeft: () => <MaterialDesignIcons name="airplane" size={25} color="#2563EB" style={{marginRight: 11}} />,
-        }}>
-          <Stack.Screen name="home" component={LandingPageScreen} />
-          <Stack.Screen name="searchFlight" component={SearchFlightPage} />
-        </Stack.Navigator>
-      </ThemeProvider>
-    </NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <ThemeProvider>
+          <Stack.Navigator initialRouteName="home" screenOptions={{
+            title: "Skyhook",
+            headerBackVisible: true,
+            headerLeft: () => <MaterialDesignIcons name="airplane" size={25} color="#2563EB" style={{marginRight: 11}} />,
+          }}>
+            <Stack.Screen name="home" component={LandingPageScreen} />
+            <Stack.Screen name="searchFlight" component={SearchFlightPage} />
+          </Stack.Navigator>
+        </ThemeProvider>
+      </NavigationContainer>
+    </SafeAreaView>
   )
 }
