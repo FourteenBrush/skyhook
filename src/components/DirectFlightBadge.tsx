6fa6bf74b6d98e9fc4d551ef4c321a3523e68cfc
FourteenBrush <74827262+FourteenBrush@users.noreply.github.com>
2025-12-01 17:05:16 +0100

    Create DirectFlightBadge

--- src/components/DirectFlightBadge.tsx
+++ src/components/DirectFlightBadge.tsx
@@ -1,7 +1,22 @@
-import { View } from "react-native";
+import { useStyleSheet } from "@/hooks/useStyleSheet"
+import { BORDER_RADIUS_ROUNDED_BUTTON, ThemeData } from "@/theme"
+import { StyleSheet, Text } from "react-native"
 
 export default function DirectFlightBadge() {
+  const styles = useStyleSheet(getStyles)
+
   return (
-    <View>Direct flight</View>
+    <Text style={styles.container}>Direct</Text>
   )
 }
+
+const getStyles = ({ fonts, colors }: ThemeData) => StyleSheet.create({
+  container: {
+    ...fonts.bodySmall,
+    backgroundColor: colors.button,
+    color: colors.buttonText,
+    paddingVertical: 3,
+    paddingHorizontal: 6,
+    borderRadius: BORDER_RADIUS_ROUNDED_BUTTON,
+  },
+})
