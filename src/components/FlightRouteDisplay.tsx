1270ee472a9f304ee9067d4bb3515763d535b598
FourteenBrush <74827262+FourteenBrush@users.noreply.github.com>
2025-12-06 23:01:53 +0100

    Create FlightRouteDisplay

--- src/components/FlightRouteDisplay.tsx
+++ src/components/FlightRouteDisplay.tsx
@@ -1,12 +1,35 @@
-import { View } from "react-native";
+import { useStyleSheet } from "@/hooks/useStyleSheet"
+import { ThemeData } from "@/theme"
+import { FontAwesome } from "@expo/vector-icons"
+import { StyleProp, StyleSheet, Text, View, ViewStyle } from "react-native"
 
 export type FlightRouteDisplayProps = {
   departure: string,
   destination: string,
+  size: "small" | "large"
+  style?: StyleProp<ViewStyle>,
 }
 
-export default function FlightRouteDisplay({ departure, destination }: FlightRouteDisplayProps) {
+export default function FlightRouteDisplay({ departure, destination, size, style }: FlightRouteDisplayProps) {
+  const styles = useStyleSheet(theme => getStyles(theme, size))
+
   return (
-    <View>Flight Route</View>
+    <View style={[styles.container, style]}>
+      <Text style={styles.text}>{departure}</Text>
+      <FontAwesome name="long-arrow-right" size={size ===  "large" ? 18 : 15} />
+      <Text style={styles.text}>{destination}</Text>
+    </View>
   )
 }
+
+const getStyles = ({ fonts }: ThemeData, size: "small" | "large") => StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    gap: 8,
+    alignItems: "center",
+  },
+  text: {
+    ...(size === "small" ? fonts.titleLarge : fonts.headlineSmall),
+    fontWeight: 500,
+  },
+})
