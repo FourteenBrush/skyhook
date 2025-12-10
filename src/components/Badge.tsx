7bd2def9d6332803037ae879b5d9132ef26b9651
FourteenBrush <74827262+FourteenBrush@users.noreply.github.com>
2025-12-10 22:18:39 +0100

    Align text vertically in Badge

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,6 +1,6 @@
 import { useStyleSheet } from "@/hooks/useStyleSheet"
 import { BORDER_RADIUS_ROUNDED_BUTTON, ThemeData } from "@/theme"
-import { StyleSheet, Text, TextProps } from "react-native"
+import { StyleSheet, Text, TextProps, View } from "react-native"
 
 export type BadgeProps = TextProps & {
   kind: "dark" | "light" | "outlined",
@@ -10,12 +10,17 @@ export default function Badge({ kind = "light", style, ...props }: BadgeProps) {
   const styles = useStyleSheet(theme => getStyles(theme, kind))
 
   return (
-    <Text style={[styles.container, style]} {...props} />
+    <View style={{ justifyContent: "center" }}>
+      <Text style={[styles.text, style]} {...props} />
+    </View>
   )
 }
 
 const getStyles = ({ fonts, colors }: ThemeData, kind: "dark" | "light" | "outlined") => StyleSheet.create({
   container: {
+    justifyContent: "center",
+  },
+  text: {
     ...fonts.bodySmall,
     color: kind === "outlined" ? colors.text : colors.buttonText,
     backgroundColor: kind === "outlined" ? undefined : (kind === "dark" ? colors.button : "#959595"),
