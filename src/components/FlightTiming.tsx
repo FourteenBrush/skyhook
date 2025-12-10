b1f75cfaea603a415625f89656ce74afcb225798
FourteenBrush <74827262+FourteenBrush@users.noreply.github.com>
2025-12-10 18:13:49 +0100

    FlightTiming: create large and small variants

--- src/components/FlightTiming.tsx
+++ src/components/FlightTiming.tsx
@@ -6,14 +6,35 @@ import { formatDurationToReadable, formatTime } from "@/utils/utils"
 import { Feather } from "@expo/vector-icons"
 import { StyleProp, StyleSheet, Text, View, ViewProps, ViewStyle } from "react-native"
 
-export default function FlightTiming({ flight }: { flight: Flight }) {
+/**
+ * Small = horizontal display of the flight departure and arrival time and airport.
+ * Large = vertical display with more details.
+ */
+export type FlightTimingKind = "small" | "large"
+
+export type FlightTimingProps = ViewProps & {
+  flight: Flight,
+  kind: FlightTimingKind,
+}
+
+export default function FlightTiming({ flight, kind, ...props }: FlightTimingProps) {
   const styles = useStyleSheet(getStyles)
 
+  const style: ViewStyle = kind === "large"
+    ? { flexDirection: "column" }
+    : { flexDirection: "row", justifyContent: "space-between" }
+
+  const [departureStyle, arrivalStyle]: [ViewStyle, ViewStyle] = [
+    { alignSelf: "flex-start" },
+    { alignSelf: "flex-end", paddingTop: kind === "large" ? 8 : 0 },
+  ]
+
   return (
-    <>
+    <View style={style} {...props}>
       <TimingSection
-        style={{ alignSelf: "flex-start" }}
-        title="Departure"
+        kind={kind}
+        style={departureStyle}
+        title={kind === "large" ? "Departure" : undefined}
         time={flight.departureTime}
         airport={flight.departureAirport}
         accessibilityHint="departure timing info"
@@ -23,32 +44,37 @@ export default function FlightTiming({ flight }: { flight: Flight }) {
         <Text accessibilityHint="total flight duration">{formatDurationToReadable(flight.totalDuration)}</Text>
       </View>
       <TimingSection
-        style={{ alignSelf: "flex-end", paddingTop: 8 }}
-        title="Arrival"
+        kind={kind}
+        style={arrivalStyle}
+        title={kind === "large" ? "Arrival" : undefined}
         time={flight.arrivalTime}
         airport={flight.arrivalAirport}
         accessibilityHint="arrival timing info"
       />
-    </>
+    </View>
   )
 }
 
 type TimingSectionProps = ViewProps & {
-  title: string,
+  kind: FlightTimingKind,
+  title?: string,
   time: Date,
   airport: Airport,
   style?: StyleProp<ViewStyle>,
 }
 
-function TimingSection({ title, time, airport, style, ...props }: TimingSectionProps) {
+function TimingSection({ kind, title, time, airport, style, ...props }: TimingSectionProps) {
   const styles = useStyleSheet(getStyles)
   const { fonts } = useTheme()
 
+  const airportName = kind === "large" ? airport.longName : airport.shortName
+
   return (
     <View style={style} {...props}>
-      <Text style={styles.title}>{title}</Text>
+      {title && <Text style={styles.title}>{title}</Text>}
       <Text style={styles.time}>{`${formatTime(time)}`}</Text>
-      <Text style={fonts.titleSmall}>{`${airport.longName}`}</Text>
+      <Text style={fonts.titleSmall}>{`${airportName}`}</Text>
+      {kind === "small" && <Text>{airport.city}</Text>}
     </View>
   )
 }
