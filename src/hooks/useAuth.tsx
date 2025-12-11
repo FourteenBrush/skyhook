9a9a9cdde52a943d75cf2de64233743357734b31
FourteenBrush <74827262+FourteenBrush@users.noreply.github.com>
2025-12-12 00:02:52 +0100

    Store user settings persistently (only one user allowed rn)

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -5,37 +5,78 @@ import * as SecureStore from "expo-secure-store"
 import { DefaultError, useMutation } from "@tanstack/react-query"
 import { Platform } from "react-native"
 import AsyncStorage from "@react-native-async-storage/async-storage"
-
-type InternalAuthState = {
-  token: string | null,
-  isLoading: boolean,
-  error: any | null,
-}
+import z from "zod"
+import _ from "lodash"
 
 export type AuthState = {
   isSignedIn: boolean,
   /** Accounts for both loading async state and actually performing network calls */
   isLoading: boolean,
-  /** Token used for authentication, always set when `isSignedIn` is true */
+  /** User authentication token, always set when `isSignedIn` is true */
   authToken: string | null,
+  /** Associated user settings, these contain defaults when the user is not signed in */
+  userSettings: UserSettings,
   /** Error which is set when both `isSignedIn` and `isLoading` are false */
   error: any | null,
   signIn: (email: string, password: string) => Promise<void>,
   signOut: () => Promise<void>,
 }
 
+export type UserSettings = {
+  preferredCurrency: CurrencyPreference,
+  /** Null indicates to rely on the device default */
+  darkMode: boolean | null,
+  defaultTripType: TripTypePreference,
+  /** A function to update a setting, is a no-op when the user is not signed in */
+  updateState: SettingsUpdateFunc,
+}
+
+export type SettingsUpdateFunc = <K extends keyof Omit<UserSettings, "updateState">>(
+  key: K,
+  value: UserSettings[K],
+) => Promise<void>
+
+const CURRENCIES = ["dollar", "euro"] as const
+export type CurrencyPreference = (typeof CURRENCIES)[number]
+const TRIP_TYPES = ["oneWay", "roundTrip"] as const
+export type TripTypePreference = (typeof TRIP_TYPES)[number]
+
+type InternalAuthState = {
+  token: string | null,
+  // we could make this nullable but it actually doesn't matter, as these
+  // will be default initialized either way
+  userSettings: UserSettings,
+  isLoading: boolean,
+  error: any | null,
+}
+
 const AuthContext = createContext<AuthState | null>(null)
 
 type AuthAction =
-  | { type: "RESTORE_TOKEN", token: string | null }
+  | { type: "RESTORE_SAVED_STATE", restoredState: SavedState | null, updateFunc: SettingsUpdateFunc }
+  | { type: "REFRESH_USER_SETTINGS", settings: UserSettings }
+  | { type: "RESTORE_TOKEN", token: string }
   | { type: "SIGN_OUT" }
   | { type: "SET_LOADING", isLoading: boolean }
   | { type: "ABORT_WITH_ERROR", error: any }
 
-const TOKEN_KEY = "__USER_TOKEN"
-
-const authReducer = (state: InternalAuthState, action: AuthAction): InternalAuthState => {
+const authStateReducer = (state: InternalAuthState, action: AuthAction): InternalAuthState => {
   switch (action.type) {
+    case "RESTORE_SAVED_STATE":
+      const { restoredState } = action
+      // use default user settings when not logged in
+      const userSettings: UserSettings = {
+        darkMode: restoredState?.darkMode ?? defaultUserSettings.darkMode,
+        defaultTripType: restoredState?.defaultTripType ?? defaultUserSettings.defaultTripType,
+        preferredCurrency: restoredState?.preferredCurrency ?? defaultUserSettings.preferredCurrency,
+        updateState: action.updateFunc,
+      }
+      const token = restoredState !== null ? restoredState.token : null
+
+      return { ...state, isLoading: false, token, userSettings }
+    case "REFRESH_USER_SETTINGS":
+      const settings: UserSettings = { ...action.settings, updateState: state.userSettings.updateState }
+      return { ...state, isLoading: false, userSettings: settings }
     case "RESTORE_TOKEN":
       return { ...state, isLoading: false, token: action.token }
     case "SIGN_OUT":
@@ -47,13 +88,21 @@ const authReducer = (state: InternalAuthState, action: AuthAction): InternalAuth
   }
 }
 
+const defaultUserSettings: UserSettings = {
+  darkMode: null,
+  defaultTripType: "roundTrip",
+  preferredCurrency: "euro",
+  updateState: async (_key, _value) => {}, // no-op as not signed in
+}
+
 /**
  * An auth context provider, the auth check is immediately started upon mounting.
  */
 export const AuthProvider = ({ children }: PropsWithChildren) => {
-  const [state, dispatch] = useReducer(authReducer, {
-    isLoading: true,
+  const [state, dispatch] = useReducer(authStateReducer, {
     token: null,
+    userSettings: { ...defaultUserSettings },
+    isLoading: true,
     error: null,
   })
 
@@ -62,25 +111,39 @@ export const AuthProvider = ({ children }: PropsWithChildren) => {
     onError: (error) => console.error("sign in failed: " + error),
   })
 
-  const validateTokenMutation = useMutation<TokenValidationResponse, DefaultError, { token: string }>({
-    mutationFn: ({ token }) => ApiClient.validateUserToken(token),
+  const validateTokenMutation = useMutation<TokenValidationResponse, DefaultError, { authToken: string }>({
+    mutationFn: ({ authToken }) => ApiClient.validateUserToken({ authToken }),
     onError: (error) => console.error("validating token failed: " + error),
   })
+
+  const updateUserSetting: SettingsUpdateFunc = async (key, value) => {
+    const newSettings = { ...state.userSettings, [key]: value }
+    await persistUserSettings(newSettings)
+    dispatch({ type: "REFRESH_USER_SETTINGS", settings: newSettings })
+  }
 
   useEffect(() => {
     const bootstrap = async () => {
       try {
-        let userToken = await PlatformStorage.getItemAsync(TOKEN_KEY)
-        if (userToken !== null) {
-          const { isValid } = await validateTokenMutation.mutateAsync({ token: userToken })
-
-          if (!isValid) {
-            userToken = null
-            await PlatformStorage.deleteValueAsync(TOKEN_KEY)
-          }
+        const restoredState = await restoreSavedState()
+        if (restoredState === null) {
+          // user might not be signed in, or an error occured while restoring state,
+          // either way, they will have to sign in again
+          dispatch({ type: "RESTORE_SAVED_STATE", restoredState: null, updateFunc: async () => {} })
+          return
         }
 
-        dispatch({ type: "RESTORE_TOKEN", token: userToken })
+        const { isValid } = await validateTokenMutation.mutateAsync({ authToken: restoredState.token })
+        if (!isValid) {
+          // only delete token, keep user settings
+          await deleteSavedToken()
+        }
+
+        dispatch({
+          type: "RESTORE_SAVED_STATE",
+          restoredState: isValid ? restoredState : null,
+          updateFunc: isValid ? updateUserSetting : async () => {},
+        })
       } catch (e) {
         dispatch({ type: "ABORT_WITH_ERROR", error: e })
       }
@@ -92,11 +155,11 @@ export const AuthProvider = ({ children }: PropsWithChildren) => {
   const signIn = async (email: string, password: string) => {
     dispatch({ type: "SET_LOADING", isLoading: true })
     try {
-      const { authToken: token } = await loginMutation.mutateAsync({ email, password })
-      await PlatformStorage.persistAsync(TOKEN_KEY, token)
-        .catch(err => console.error("failed to persist user token after sign in: " + err))
+      const { authToken } = await loginMutation.mutateAsync({ email, password })
+      await persistLoginState(authToken)
+        .catch(err => console.error("failed to persist auth state after sign in: " + err))
 
-      dispatch({ type: "RESTORE_TOKEN", token })
+      dispatch({ type: "RESTORE_TOKEN", token: authToken })
     } catch (e) {
       dispatch({ type: "ABORT_WITH_ERROR", error: e })
     }
@@ -116,6 +179,7 @@ export const AuthProvider = ({ children }: PropsWithChildren) => {
     isSignedIn: state.token !== null,
     isLoading: state.isLoading,
     authToken: state.token,
+    userSettings: state.userSettings,
     error: state.error,
     signIn,
     signOut,
@@ -134,6 +198,50 @@ export const useAuth = (): AuthState  => {
     throw new Error("AuthContext Provider not found in the component tree")
   }
   return ctx
+}
+
+const savedStateSchema = z.object({
+  preferredCurrency: z.enum(CURRENCIES),
+  defaultTripType: z.enum(TRIP_TYPES),
+  darkMode: z.boolean().nullable(),
+})
+type SavedState = z.infer<typeof savedStateSchema> & {
+  token: string,
+}
+
+const TOKEN_KEY = "__TOKEN"
+const SETTINGS_KEY = "__SETTINGS"
+
+const restoreSavedState = async (): Promise<SavedState | null> => {
+  const token = await PlatformStorage.getItemAsync(TOKEN_KEY)
+  if (token === null) return null
+  const settings = await PlatformStorage.getItemAsync(SETTINGS_KEY)
+  if (settings === null) return null
+
+  const decodedSettings = JSON.parse(settings)
+  return {
+    ...savedStateSchema.parse(decodedSettings),
+    token,
+  }
+}
+
+const persistLoginState = async (token: string) => {
+  const [_, encodedSettings] = await Promise.all([
+    PlatformStorage.persistAsync(TOKEN_KEY, token),
+    PlatformStorage.getItemAsync(SETTINGS_KEY),
+  ])
+
+  if (encodedSettings === null) {
+    // first time a user signed in, save default settings
+    await PlatformStorage.persistAsync(SETTINGS_KEY, JSON.stringify(defaultUserSettings))
+  }
+}
+
+const deleteSavedToken = async (): Promise<void> =>
+  PlatformStorage.deleteValueAsync(TOKEN_KEY)
+
+const persistUserSettings = async (settings: UserSettings): Promise<void> => {
+  await PlatformStorage.persistAsync(SETTINGS_KEY, JSON.stringify(settings))
 }
 
 // NOTE: web does not support secure store, use localStorage instead
--- src/screens/account/SettingsTab.tsx
+++ src/screens/account/SettingsTab.tsx
@@ -1,19 +1,22 @@
 import Card from "@/components/Card"
 import RoundedIconBackground from "@/components/RoundedIconBackground"
 import TextButton from "@/components/TextButton"
+import { CurrencyPreference, useAuth } from "@/hooks/useAuth"
 import { useStyleSheet } from "@/hooks/useStyleSheet"
 import { useTheme } from "@/hooks/useTheme"
 import { CONTAINER_MARGIN, ThemeData } from "@/theme"
-import { FontAwesome, Octicons } from "@expo/vector-icons"
-import { useState } from "react"
+import { Feather, FontAwesome, MaterialIcons, Octicons } from "@expo/vector-icons"
+import { ReactNode } from "react"
 import { StyleSheet, Switch, Text, View } from "react-native"
 
 export default function SettingsTab() {
   const styles = useStyleSheet(getStyles)
   const { colors, fonts } = useTheme()
-  const [bool, setBool] = useState(false)
+  const { signOut, userSettings } = useAuth()
+
 
   // TODO: default booking name, currency icon, theme, direct or indirect flight default
+  console.warn("SettingsTab settings:", userSettings)
 
   return (
     <View style={styles.container}>
@@ -30,20 +33,75 @@ export default function SettingsTab() {
           </View>
         </View>
 
-        <TextButton kind="outlined">
+        <TextButton kind="outlined" onPress={signOut}>
           <View style={{ flexDirection: "row", alignItems: "center" }}>
             <FontAwesome name="sign-out" size={18} style={{ opacity: 0.60, marginRight: 10 }} />
             <Text>Sign Out</Text>
           </View>
         </TextButton>
       </Card>
-      <Switch
-        value={bool}
-        onValueChange={setBool}
-        trackColor={{ false: '#767577', true: '#3B82F6' }} // blue
-        thumbColor={bool ? '#fff' : '#f4f3f4'}
+
+      {/* FIXME: show error label when updateState failed */}
+      <SettingsCard<CurrencyPreference>
+        title="Currency"
+        subtitle="Select your preferred currency for prices"
+        leftOption={{ value: "dollar", text: "USD", icon: ({ color }) => <Feather name="dollar-sign" color={color} size={24} /> }}
+        rightOption={{ value: "euro", text: "EUR", icon: ({ color }) => <MaterialIcons name="euro-symbol" color={color} size={24} /> }}
+        selection={userSettings.preferredCurrency === "dollar" ? "left" : "right"}
+        onChange={userSettings.updateState.bind(null, "preferredCurrency")}
       />
     </View>
+  )
+}
+
+type SettingsCardProps<T> = {
+  title: string,
+  subtitle: string,
+  leftOption: { value: T, icon: ({ color }: { color: string | undefined }) => ReactNode, text: string },
+  rightOption: { value: T, icon: ({ color }: { color: string | undefined }) => ReactNode, text: string },
+  selection: "left" | "right",
+  onChange: (value: T) => void,
+}
+
+/**
+ * @template T the type stored in every option
+ */
+const SettingsCard = <T,>({ title, subtitle, leftOption, rightOption, selection, onChange }: SettingsCardProps<T>) => {
+  const styles = useStyleSheet(getStyles)
+  const { fonts, colors } = useTheme()
+
+  const [leftIconColor, rightIconColor] = selection === "left"
+    ? [colors.primary, undefined]
+    : [undefined, colors.primary]
+
+  return (
+    <Card clickable={false}>
+      <Text style={styles.settingsTitle}>{title}</Text>
+      <Text style={styles.settingsSubtitle}>{subtitle}</Text>
+
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <View style={styles.settingsOption}>
+          <RoundedIconBackground color={colors.primaryShaded} size={38}>
+            {leftOption.icon({ color: leftIconColor })}
+          </RoundedIconBackground>
+          <Text style={fonts.titleMedium}>{leftOption.text}</Text>
+        </View>
+
+        <Switch
+          value={selection === "right"}
+          onValueChange={(right) => onChange(right ? rightOption.value : leftOption.value)}
+          trackColor={{ false: '#767577', true: '#3B82F6' }} // blue
+          thumbColor={selection === "left" ? '#fff' : '#f4f3f4'}
+        />
+
+        <View style={styles.settingsOption}>
+          <Text style={fonts.titleMedium}>{rightOption.text}</Text>
+          <RoundedIconBackground color={colors.primaryShaded} size={38}>
+            {rightOption.icon({ color: rightIconColor })}
+          </RoundedIconBackground>
+        </View>
+      </View>
+    </Card> 
   )
 }
 
@@ -60,5 +118,21 @@ const getStyles = ({ colors, fonts }: ThemeData) => StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     gap: 9,
+  },
+
+  settingsTitle: {
+    ...fonts.titleMedium,
+    fontWeight: 600,
+    marginBottom: 6,
+  },
+  settingsSubtitle: {
+    ...fonts.titleSmall,
+    color: colors.textSecondary,
+    marginBottom: 12,
+  },
+  settingsOption: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 6,
   },
 })
